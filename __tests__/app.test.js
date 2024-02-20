9c84f94354f9ee11a2db2cec13d07aa6cec3f2d8
Rayhan Elbeera <raybeera2014@gmail.com>
2024-02-20 15:29:32 +0000

    cpmpleted task 5

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -83,25 +83,25 @@ describe("GET /api/articles/:article_id", () => {
   });
 });
 
-xdescribe("GET /api/articles", () => {
+describe("GET /api/articles", () => {
   it("return an array of object with all articles", () => {
     return request(app)
       .get("/api/articles")
       .expect(200)
       .then((res) => {
         const { articles } = res.body;
+        console.log(res.body)
         expect(articles.length).toBe(13);
         articles.forEach((article) => {
           expect(article).toMatchObject({
             author: expect.any(String),
             title: expect.any(String),
             article_id: expect.any(Number),
-            body: expect.any(String),
             topic: expect.any(String),
             created_at: expect.any(String),
             votes: expect.any(Number),
             article_img_url: expect.any(String),
-            comment_count: expect.any(Number),
+            comment_count: expect.any(String),
           });
         });
       });
--- app/app.model.js
+++ app/app.model.js
@@ -37,11 +37,16 @@ function selectArticleById(articles_id) {
 function selectAllArticles() {
   return db
     .query(
-      `Select * 
-  From articles`
+      `SELECT articles.author, title, articles.article_id, topic, articles.created_at, articles.votes, article_img_url,
+  COUNT (comments.comment_id) AS comment_count
+  FROM articles
+  LEFT JOIN comments
+  ON articles.article_id = comments.article_id
+  GROUP BY articles.article_id
+  ORDER BY articles.created_at DESC;`
     )
     .then((article) => {
-      article.rows;
+      return article.rows;
     });
 }
 module.exports = {
